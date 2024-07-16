509b270bc72e494bd157315dd63545aa33262154
jenniferWitzig <157106266+jenniferWitzig@users.noreply.github.com>
2024-07-16 17:45:13 +0200

    clean up

--- agentmanager/frontend/src/components/YamlPreviewer.vue
+++ agentmanager/frontend/src/components/YamlPreviewer.vue
@@ -2,10 +2,15 @@
   <div class="ring-gray-900/5 sm:mx-0 lg:col-span-2 lg:row-span-2 ">
 <!--    <h2 class="text-base font-semibold leading-6 text-gray-900">Konfiguration</h2>-->
     <div class="bg-gray-800 text-white p-4 rounded-lg shadow-md">
-      <pre><code ref="code" class="yaml">{{ code }}</code></pre>
+      <pre v-highlightjs="code" class="yaml">
+<!--        <code ref="code" class="yaml">-->
+          {{ code }}
+<!--        </code>-->
+      </pre>
     </div>
   </div>
 </template>
+
 <script>
 import {onMounted, ref} from 'vue';
 import hljs from 'highlight.js/lib/core';
@@ -22,20 +27,33 @@ export default {
       required: true
     }
   },
-  setup(props, {emit}) {
-    const codeElement = ref(null);
-
+  setup(props) {
     onMounted(() => {
-      if (codeElement.value) {
-        codeElement.value.textContent = props.code;
-        hljs.highlightBlock(codeElement.value);
-      }
+      hljs.highlightAll();
     });
 
-    return {
-      codeElement
-    };
+    return {};
   }
+  // props: {
+  //   code: {
+  //     type: String,
+  //     required: true
+  //   }
+  // },
+  // setup(props, {emit}) {
+  //   const codeElement = ref(null);
+  //
+  //   onMounted(() => {
+  //     if (codeElement.value) {
+  //       codeElement.value.textContent = props.code;
+  //       hljs.highlightBlock(codeElement.value);
+  //     }
+  //   });
+  //
+  //   return {
+  //     codeElement
+  //   };
+  // }
 };
 </script>
 
