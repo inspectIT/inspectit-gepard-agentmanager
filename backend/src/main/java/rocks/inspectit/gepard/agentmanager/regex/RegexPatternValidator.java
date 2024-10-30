bda312f29906150093f620669bb6032389f336e4
Levin Kerschberger <Levin.Kerschberger@novatec-gmbh.de>
2024-10-30 09:42:40 +0100

    feat: write "regex:" in query-param to indicate that the query contains a regExp

---
+++ backend/src/main/java/rocks/inspectit/gepard/agentmanager/regex/RegexPatternValidator.java
@@ -0,0 +1,33 @@
+/* (C) 2024 */
+package rocks.inspectit.gepard.agentmanager.regex;
+
+import static rocks.inspectit.gepard.agentmanager.regex.RegexQueryService.REGEX_INDICATOR;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
+/**
+ * Validates that a given string is a valid regex pattern.
+ *
+ * <p>Used in conjunction with the {@link ValidRegexPattern} annotation.
+ */
+public class RegexPatternValidator implements ConstraintValidator<ValidRegexPattern, String> {
+
+  @Override
+  public boolean isValid(String value, ConstraintValidatorContext context) {
+    if (value == null) {
+      return true;
+    }
+    if (value.startsWith(REGEX_INDICATOR)) {
+      try {
+        Pattern.compile(value.substring(REGEX_INDICATOR.length()));
+        return true;
+      } catch (PatternSyntaxException e) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
